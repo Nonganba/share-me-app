b14084a3278d0e3bfe9167173031f0e19e9bbdcd
Nonganba <nanganba.th1109@gmail.com>
2022-12-20 17:13:41 +0530

    add Sidebar

--- share-me-frontend/src/App.js
+++ share-me-frontend/src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./container/Home";
+import Pins from "./container/Pins";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/*" element={<Home />} />
       <Route path="login" element={<Login />} />
     </Routes>
   );
